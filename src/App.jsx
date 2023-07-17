81f3251f28b8e97a4952f760f0c588635e221b95
asasada <terrywu50@hotmail.com>
2023-07-17 21:17:15 +0900

    added title, sidebar, memo input form with basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,20 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-	const [count, setCount] = useState(0)
+	const [title, setTitle] = useState(0)
+	const [body, setBody] = useState(0)
 
 	return (
 	<>
+		<header className='text-center text-2xl py-4'>React App</header>
+		<aside className='fixed left-0 z-40 w-64 h-screen'>
+			<li>React</li>
+			<li>React Router</li>
+		</aside>
+		<main className='h-screen mx-64 flex flex-col items-stretch'>
+			<input type='text' value={title} onChange={e => setTitle(e.target.value)} />
+			<input type='text' value={body} onChange={e => setBody(e.target.value)} />
+		</main>
 	</>
 	)
 }
