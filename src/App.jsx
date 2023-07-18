a229dc07bff20f64f4290743d3710f724c33abfe
asasada <terrywu50@hotmail.com>
2023-07-18 23:24:25 +0900

    added new page button. it saves the current page to local storage and shows the title in the sidebar. then it clears the current page for new input.

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,35 @@ import './App.css'
 function App() {
 	const [title, setTitle] = useState('')
 	const [body, setBody] = useState('')
+	const [pages, setPages] = useState([])
 
+	function newPage() {
+		var date = new Date(Date.now())
+		setPages(currentPages => {
+			return [...currentPages, { id: crypto.randomUUID(),
+				title,
+				body,
+				last_updated: date.toUTCString() }]
+		})
+		localStorage.setItem('pages', JSON.stringify(pages))
+		setTitle('')
+		setBody('')
+	}
+
+	console.log(pages)
 	return (
 	<>
 		<header className='text-center text-2xl py-4'>React App</header>
 		<aside className='fixed left-0 z-40 w-64 h-screen'>
-			<li>React</li>
-			<li>React Router</li>
+			<button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'
+				onClick={newPage}>
+				New Page
+			</button>
+			<ul>
+				{pages.map(page => {
+					return <li key={page.id}>{page.title}</li>
+				})}
+			</ul>
 		</aside>
 		<main className='h-screen mx-64 flex flex-col items-stretch'>
 			<input type='text' value={title} onChange={e => setTitle(e.target.value)} 
